6d43e99789fff7754d495321b2cb5b74ae9ca2cb
samilairadukunda <samilairadukunda@gmail.com>
2024-10-31 19:49:30 +0100

    feature(nav):navigation menu
     -nav items transilation
    
     [Delivers #6]

--- js/main.js
+++ js/main.js
@@ -2,10 +2,18 @@ const translations = {
     en: {
         name: "Samila Iradukunda",
         headerDescription: "Computer Science Student | Germany",
+        about: "About",
+        skills: "Skills",
+        projects: "Projects",
+        contact: "Contact",
     },
     de: {
         name: "Samila Iradukunda ",
         headerDescription: "InformatikStudent | DeutschFach",
+        about: "Um",
+        skills: "Fähigkeiten",
+        projects: "Projekt",
+        contact: "kontakt",
     },
 };
 
@@ -21,4 +29,9 @@ function updateContent(language) {
     document.getElementById("name").innerText = translations[language].name;
     document.getElementById("header-description").innerText =
         translations[language].headerDescription;
+    document.getElementById("about").innerText = translations[language].about;
+    document.getElementById("skills").innerText = translations[language].skills;
+    document.getElementById("projects").innerText =
+        translations[language].projects;
+    document.getElementById("contact").innerText = translations[language].contact;
 }
