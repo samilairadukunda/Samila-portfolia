8d6220e461f517706eec926cf0d717ae848639de
samilairadukunda <samilairadukunda@gmail.com>
2024-11-19 20:30:57 +0100

    feature(about):me (#13)
    - translate about me title
    
    [Delivers #10]

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ const translations = {
         skills: "Skills",
         projects: "Projects",
         contact: "Contact",
+        aboutTitle: "About Me",
+        aboutDescription: " I am a computer science student currently studying in Germany. I was born in Rwanda and have a passion for technology, coding, and problem-solving. ",
     },
     de: {
         name: "Samila Iradukunda ",
@@ -14,6 +16,8 @@ const translations = {
         skills: "Fähigkeiten",
         projects: "Projekt",
         contact: "kontakt",
+        aboutTitle: "Über mich",
+        aboutDescription: "Ich bin Informatikstudentin, die derzeit in Deutschland studiert. Ich bin in Ruanda geboren und ich habe eine Leidenschaft für Technologie, Programmierung und Problemlösung.",
     },
 };
 
@@ -34,4 +38,8 @@ function updateContent(language) {
     document.getElementById("projects").innerText =
         translations[language].projects;
     document.getElementById("contact").innerText = translations[language].contact;
+    document.getElementById("about-title").innerText =
+        translations[language].aboutTitle;
+    document.getElementById("about-description").innerText =
+        translations[language].aboutDescription;
 }
